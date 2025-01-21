50bb4ed7d13dff4207e66358c21466e74d87a0ba
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-01-21 11:24:59 -0500

    fixed user input validations

--- lab03.py
+++ lab03.py
@@ -8,17 +8,23 @@ weapons = ['Fist', 'Knife', 'Club', 'Gun', 'Bomb', 'Nuclear Bomb']
 
 print("Available Weapons:", ', '.join(weapons))
 
-# Inputs
-combatStrength = max(1, min(6, int(input("Hero strength (1-6): "))))
+# # Inputs for combat strength of hero
+combatStrength = int(input("Enter your combat strength (1-6): "))
+if combatStrength < 1 or combatStrength > 6:
+    print("Invalid Combat Strength , please select between 1-6")
+    # default value for invalid input
+    combatStrength = 1
 
-mCombatStrength = max(1, min(6, int(input("Monster strength (1-6): "))))
-
-# Battle
-for j in range(1, 21, 2):
-    heroRoll, monsterRoll = random.choice(diceOptions), random.choice(diceOptions)
-    heroTotal, monsterTotal = combatStrength + heroRoll, mCombatStrength + monsterRoll
-    print(f"Round {j}: Hero({weapons[heroRoll - 1]})={heroTotal}, Monster({weapons[monsterRoll - 1]})={monsterTotal}.",
-          "Hero wins!" if heroTotal > monsterTotal else "Monster wins!" if heroTotal < monsterTotal else "Tie!")
-    if j == 11:
-        print("Battle Truce declared. Game Over!")
-        break
+# combatStrength = max(1, min(6, int(input("Hero strength (1-6): "))))
+#
+# mCombatStrength = max(1, min(6, int(input("Monster strength (1-6): "))))
+#
+# # Battle
+# for j in range(1, 21, 2):
+#     heroRoll, monsterRoll = random.choice(diceOptions), random.choice(diceOptions)
+#     heroTotal, monsterTotal = combatStrength + heroRoll, mCombatStrength + monsterRoll
+#     print(f"Round {j}: Hero({weapons[heroRoll - 1]})={heroTotal}, Monster({weapons[monsterRoll - 1]})={monsterTotal}.",
+#           "Hero wins!" if heroTotal > monsterTotal else "Monster wins!" if heroTotal < monsterTotal else "Tie!")
+#     if j == 11:
+#         print("Battle Truce declared. Game Over!")
+#         break
